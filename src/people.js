365532d39a734a6533a06da490e7a68c47e5cc58
Eros Max <erosmsc2@gmail.com>
2025-10-21 20:22:55 -0300

    commit legal

---
+++ src/people.js
@@ -0,0 +1,21 @@
+const people = [
+    {
+        name: "Carlinhos",
+        age: 69,
+        email: "carlinhos2050cavalos@gmail.com"
+    },
+
+    {
+        name: "Eros Max",
+        age: 16,
+        email: "eros#max@gmail.com"
+    },
+
+    {
+        name: "Corpo em Ação",
+        age: 9,
+        email: "corpoemacao@gmail.com"
+    }
+]
+
+export { people }
