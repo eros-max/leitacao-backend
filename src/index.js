16ece3974978f9b3e4811a714a984b68d651f6e0
Eros Max <erosmsc2@gmail.com>
2025-11-06 20:52:34 -0300

    Envio de Informações do frontend para o backend #1 e #2, quase realizado

--- src/index.js
+++ src/index.js
@@ -7,8 +7,23 @@ const port = 3333
 
 app.use(cors())
 
+app.use(express.json())
+
 app.get("/", (request, response) => {
     response.json(people)
+})
+
+app.post("/cadastrar", (request, response) => {
+    const { name, email, age, password } = request.body.user
+
+    console.log(`
+        Nome: ${name},
+        Senha: ${password},
+        E-mail: ${email},
+        Idade: ${age}
+    `)
+
+    response.status(201).json({ message: "Usuário Cadastrado com Sucesso!"})
 })
 
 app.listen(port, () => {
